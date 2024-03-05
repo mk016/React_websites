b8e388afc897868562af2fe4e292b0df7ff36ced
Mahendra Kumawat <mahendrakumawat80224@gmail.com>
2024-03-05 20:43:20 +0530

    some other updater for page

--- src/Components /Hero/Hero.js
+++ src/Components /Hero/Hero.js
@@ -3,21 +3,21 @@ import arrow_btn from '../../assets/arrow_btn.png'
 import play_icon from '../../assets/play_icon.png'
 import pause_icon from'../../assets/pause_icon.png'
 
-const Hero = ({ heroData, setHeroCount, heroCount, setPlayStatus, playStatus }) => {
+const Hero = ({ heroData, heroCount, setHeroCount, setPlayStatus, playStatus }) => {
   return (
     <div className='hero'>
       <div className='hero-text'>
-        <p>{heroData.text1}</p>
-        <p>{heroData.text2}</p>
+        <p>{heroData[heroCount].text1}</p>
+        <p>{heroData[heroCount].text2}</p>
         <div className='hero-explore'>
           <p> Explore the features</p>
           <img src={arrow_btn} alt='' />
         </div>
         <div className='hero-dot-play'>
           <ul className='hero-dots'>
-            <li onClick={() => setHeroCount(1)} className={heroCount === 0 ? "hero-dot orange" : "hero-dot"}></li>
-            <li onClick={() => setHeroCount(2)} className={heroCount === 1 ? "hero-dot orange" : "hero-dot"}></li>
-            <li onClick={() => setHeroCount(3)} className={heroCount === 2 ? "hero-dot orange" : "hero-dot"}></li>
+            <li onClick={() => setHeroCount(0)} className={heroCount === 0 ? "hero-dot orange" : "hero-dot"}></li>
+            <li onClick={() => setHeroCount(1)} className={heroCount === 1 ? "hero-dot orange" : "hero-dot"}></li>
+            <li onClick={() => setHeroCount(2)} className={heroCount === 2 ? "hero-dot orange" : "hero-dot"}></li>
           </ul>
           <div className='hero-play'>
             <img onClick={() => setPlayStatus(!playStatus)} src={playStatus ? pause_icon : play_icon} alt='' />
@@ -30,3 +30,9 @@ const Hero = ({ heroData, setHeroCount, heroCount, setPlayStatus, playStatus }) 
 }
 
 export default Hero
+
+const heor2 = (()=>{
+  return
+  
+  
+})
